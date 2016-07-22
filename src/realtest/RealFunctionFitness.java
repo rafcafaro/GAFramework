5ff593ea9b40943d9fafd24742a0665b55675ddb
alenic <a.nicolosi.somm@vitrociset.it>
2016-07-22 14:40:13 +0200

    Modified Clone of RealFunctionFitness, added article folder with papers

--- src/realtest/RealFunctionFitness.java
+++ src/realtest/RealFunctionFitness.java
@@ -8,7 +8,7 @@ public class RealFunctionFitness implements FitnessFunction<RealGenotype> {
 	@Override
 	public double calculateFitness(RealGenotype genotype) {
 		// Calculate the function f(x) = x[1]^2 + x[2]^2 + ... + x[n]^2
-		double[] genes = genotype.getGenes().clone();
+		double[] genes = genotype.getGenes();
 		double f=0; // value of the function
 		for (int i = 0; i < genes.length; i++) {
 			f += genes[i]*genes[i];
