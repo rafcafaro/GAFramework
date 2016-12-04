e6d1bac85389a3d63113c6937cf7316423a660ed
raf.cafaro <raf.cafaro88@gmail.com>
2016-12-04 18:21:03 +0200

    formatting real genotype test

--- src/realtest/GAFrameworkRealTest.java
+++ src/realtest/GAFrameworkRealTest.java
@@ -14,21 +14,23 @@ public class GAFrameworkRealTest {
 		int populationSize = 20;
 		int numberOfPopulations = 1;
 		int numberOfGenes = 10;
-		double targetFitness = 0.0000001;
+		double targetFitness = 1e-7;
 		boolean isDebugActive = true;
 		long debugPrintGenerations = 100000L;
 
 		GeneticAlgorithm<RealGenotype> algorithm;
 		ArrayList<Population<RealGenotype>> populations = new ArrayList<Population<RealGenotype>>();
-		
-		for (int i = 0; i < numberOfPopulations; i++) {		
+
+		for (int i = 0; i < numberOfPopulations; i++) {
 			RealGenotype[] genotypes = new RealGenotype[populationSize];
 			for (int j = 0; j < populationSize; j++) {
 				genotypes[j] = new RealGenotype(numberOfGenes);
 			}
-			populations.add(new Population<RealGenotype>(genotypes, new RouletteWheelSelection(),new RealFunctionFitness()));
+			populations.add(
+					new Population<RealGenotype>(
+							genotypes, new RouletteWheelSelection(), new RealFunctionFitness()));
 		}
-		long startTime=0,endTime = 0;
+		long startTime = 0, endTime = 0;
 		algorithm = new GeneticAlgorithm<>(populations, targetFitness);
 		algorithm.setDebug(isDebugActive, debugPrintGenerations);
 		algorithm.start();
@@ -41,16 +43,15 @@ public class GAFrameworkRealTest {
 			e.printStackTrace();
 		}
 		Genotype solution = algorithm.getBestSolutionFound();
-		if(solution != null)
-		{
-			double [] genes = ((RealGenotype)solution).getGenes();
+		if (solution != null) {
+			double[] genes = ((RealGenotype) solution).getGenes();
 			for (int i = 0; i < genes.length; i++) {
-				System.out.print(genes[i]+" ");
+				System.out.print(genes[i] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println("Elapsed time (milliseconds): "+(endTime - startTime));
-		
+		System.out.println("Elapsed time (milliseconds): " + (endTime - startTime));
+
 	}
-	
+
 }
